a14dc8255ee50ce5493260be6f9a263bae199ef8
l-errony <rudakovavaleria91@gmail.com>
2022-12-22 22:07:16 +0300

    add files

--- src/components/Header/index.js
+++
@@ -1,1 +0,0 @@
-export {defaullt} from "./Header"
--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import favoriteReducer from './favoriteReducers'
-
-export default combineReducers({
-    favoriteReducer
-    })
